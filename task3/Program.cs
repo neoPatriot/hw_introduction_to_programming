516571feced9d77d83c0a487d91a2d1e1b07bdaf
Aleksey Utkin <neopatriot@mail.ru>
2024-03-12 11:19:22 +0300

    added task3

---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 3: Задайте произвольный массив. 
+//Выведете его элементы, начиная с конца. 
+//Использовать рекурсию, не использовать циклы.
+
+char[] array = {'!','т','е','в','и','р','П'};
+
+void Print (char[] array, int i)
+{
+    if(i < 0) return;
+    Console.Write(array[i]);
+    Print(array, i - 1);
+}
+
+Print(array, array.Length - 1);
