62f4df1b05a9e0e0053c5462c789037207804f00
Aleksey Utkin <neopatriot@mail.ru>
2024-03-12 09:34:29 +0300

    added task1

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 1: Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+//Использовать рекурсию, не использовать циклы.
+
+void Numbers (int min, int max)
+{
+    if(max<min) return;
+    Numbers(min, max-1);
+    Console.Write($"{max} ");
+}
+
+Console.WriteLine("Введите натуральное число M (Начало отрезка)");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число N (Конец отрезка)");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n>m){
+Numbers(m, n);
+} else {
+    Console.WriteLine("Число N должно быть больше M");
+}
