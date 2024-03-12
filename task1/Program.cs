cf623e5a4317f737a413ecef72ba05706cb3957d
Aleksey Utkin <neopatriot@mail.ru>
2024-03-12 10:46:29 +0300

    added task2

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,19 +4,22 @@
 
 void Numbers (int min, int max)
 {
-    if(max<min) return;
+    if(max < min) return;
     Numbers(min, max-1);
     Console.Write($"{max} ");
 }
 
-Console.WriteLine("Введите натуральное число M (Начало отрезка)");
+Console.WriteLine("Введите натуральное число M");
 int m = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите натуральное число N (Конец отрезка)");
+Console.WriteLine("Введите натуральное число N");
 int n = Convert.ToInt32(Console.ReadLine());
 
-if(n>m){
-Numbers(m, n);
-} else {
-    Console.WriteLine("Число N должно быть больше M");
+if(n > m)
+{
+    Numbers(m, n);
+} 
+else 
+{
+    Numbers(n, m);
 }
---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+
+Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Akkerman (int m, int n)
+{
+    if(m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0) 
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else 
+    {
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+    }
+}
+
+Console.WriteLine($"A({m}, {n}) =  {Akkerman(m, n)}");
